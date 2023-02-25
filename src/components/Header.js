381fa7d3e9d526dabfd8635549d2c4b504569c67
Danilo Kulisic <danilokulisic4@gmail.com>
2023-02-25 15:35:50 +0100

    adding React.js

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import react from 'react';
+
+function Header(){
+    return(
+        <div>
+                <meta charSet="utf-8" />
+                <meta name="view-port" content="width=device-width , initial-scale=1 , viewport-fit= cover" />
+                <link rel="stylesheet" href="Portfolio.css" />
+                <script
+                   src="https://kit.fontawesome.com/c144e9175f.js"
+                   crossOrigin="true"
+                ></script>
+                <title>Danilo Kulisic</title>
+        </div>
+    )
+}
+
+export default Header;
